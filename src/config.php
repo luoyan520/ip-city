f103e9bc809f9b4ea0a7738b70c3420aa5f55afc
LuoYan <51085726@qq.com>
2020-05-02 15:24:35 +0800

    update

--- src/config.php
+++ src/config.php
@@ -7,9 +7,9 @@ use think\facade\Env;
 
 return [
     // 高德地图API秘钥
-    'amap_api_key'       =>    Env::get('IpCity.Amap_ApiKey', ''),
+    'amap_api_key'       =>    Env::get('IpCity.AmapApiKey', ''),
 
     // 腾讯地图接口
-    'tencent_api_key'    =>    Env::get('IpCity.Tencent_ApiKey', ''),
-    'tencent_sign_str'   =>    Env::get('IpCity.Tencent_SignStr', ''),
+    'tencent_api_key'    =>    Env::get('IpCity.TencentApiKey', ''),
+    'tencent_sign_str'   =>    Env::get('IpCity.TencentSignStr', ''),
 ];
